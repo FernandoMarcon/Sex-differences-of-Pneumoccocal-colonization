e5241ee39278d26df6fd695fb20622afad3e407d
FernandoMarcon <fmarcon13@gmail.com>
2021-04-27 20:01:12 -0300

    added loop for all datasets

--- src/workflow.R
+++ src/workflow.R
@@ -24,6 +24,11 @@ removeUnpairedSamples <- function(dataset) {
   selected_vol = num_samples %>% filter(num_samples > 1) %>% .$volunteer_id
   dataset$pheno <- pheno[which(pheno$volunteer_id %in% selected_vol),]
   dataset$counts <- dataset$counts[,rownames(dataset$pheno)]
+
+  dataset$pheno$timepoint <- as.factor(dataset$pheno$timepoint) %>% relevel(ref = 'baseline')
+  dataset$pheno$class <- as.factor(dataset$pheno$class)
+  dataset$pheno$volunteer_id <- as.factor(dataset$pheno$volunteer_id)
+
   message('Removed:\n')
   num_samples %>% filter(num_samples == 1) %>% print
   testConsistency(dataset)
@@ -37,17 +42,46 @@ filterCounts <- function(dataset) {
   return(dataset)
 }
 
-dataset.name = 'Adults1'
-dataset <- loadDataset(dataset.name)
-dataset <- removeUnpairedSamples(dataset)
-dataset <- filterCounts(dataset)
-runDE <- function(dataset, de.method = c('edgeR','DESeq2')){
+generateDesignMtx <- function(dataset) {
+  pheno <- dataset$pheno
+  dataset$design.mtx <- lapply(levels(pheno$class), function(class.name) {
+    data.frame(ifelse(pheno$class == class.name & pheno$timepoint != 'baseline', 1, 0))
+    }) %>% Reduce(cbind, .) %>% setNames(levels(pheno$class)) %>% as.matrix %>%
+    cbind(model.matrix(~ volunteer_id, data = pheno))
+  return(dataset)
+  # temp = design.mtx %>% as.data.frame %>% filter(Adults1_NEG_F == 1) %>% row.names
+  # table(pheno[temp,c('class', 'timepoint')])
+  }
 
+runDE <- function(dataset, FUN = ...){
+  res <- FUN(dataset)
+  return(res)
 }
 
 runDESeq2 <- function(dataset) {
-  dds <- DESeq2::DESeqFromMatrix(dataset$counts, dataset$pheno, design = '~ 1')
-  return(dds)
+  dds <- DESeq2::DESeqDataSetFromMatrix(dataset$counts, dataset$pheno, design = dataset$design)
+  dds <- DESeq2::DESeq(dds, parallel = T)
+  de.res <- lapply(levels(dataset$pheno$class), function(class.name) {
+    res <- results(dds, name = class.name)
+    res %>% as.data.frame %>% mutate(class = class.name) %>%
+    rownames_to_column('gene_id') %>%
+    rename(log2FoldChange = 'logFC')
+  }) %>% setNames(levels(dataset$pheno$class))
+  return(de.res)
 }
 
-dds <- runDESeq2(dataset)
+#--- RUN: DESeq2
+datasets <- c('Adults1','Adults2','Adults3','Elderly1')
+res.all <- lapply(datasets, function(dataset.name){
+  # dataset.name = 'Adults1'
+  message('\n\n',dataset.name, '\n\n')
+  dataset <- loadDataset(dataset.name)
+  dataset <- removeUnpairedSamples(dataset)
+  dataset <- filterCounts(dataset)
+  dataset <- generateDesignMtx(dataset)
+  res <- runDE(dataset, runDESeq2)
+  return(res)
+  }) %>% unlist(res.all, recursive = F)
+
+
+names(res.all)
