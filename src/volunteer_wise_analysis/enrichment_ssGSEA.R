8fb9ef475d65d809f953fb88e30ce1437c72de6d
FernandoMarcon <fmarcon13@gmail.com>
2021-05-05 13:04:14 -0300

    added recursivity to ssGSEA

--- src/volunteer_wise_analysis/enrichment_ssGSEA.R
+++ src/volunteer_wise_analysis/enrichment_ssGSEA.R
@@ -1,23 +1,27 @@
 rm(list = ls())
 pkgs <- c('tidyverse')
 suppressPackageStartupMessages(sapply(pkgs, require, character.only = T))
-basedir = 'intermediate/volunteer_wise_analysis'
-gtm.dbs <- list.files('data/GTM')
 
 #### ============================================= ssGSEA ============================================= ####
-gtm.db <- gtm.dbs[1]
-outdir <- file.path(basedir, 'ssGSEA', gtm.db)
-if(!dir.exists(outdir)) dir.create(outdir, recursive = T)
-
-file.copy(file.path(basedir, 'logFC_geneClean.csv'),outdir)
-setwd(outdir)
-
-# run Single_Sample_GSEA_ssGSEA_fgsea.R
 prjt_path <- '/home/marcon/Documents/work/Sex-differences-of-Pneumoccocal-colonization'
-source(file.path(prjt_path,'src/Single_Sample_GSEA_ssGSEA_fgsea.R'))
-gmtfile <- file.path(prjt_path,'data/GTM/',gtm.db) # Reactome_2016
+basedir = file.path(prjt_path, 'intermediate','volunteer_wise_analysis')
+gtm.dbs <- list.files(file.path(prjt_path, 'data','GTM'), full.names = T)
+
 fileranks <- "logFC_geneClean.csv"
 Ptype <- "padj"
 pval_cutoff <- 0.1
-ssGSEA(gmtfile=gmtfile,fileranks=fileranks,Ptype=Ptype,pval_cutoff=pval_cutoff)
-file.remove('logFC_geneClean.csv')
+
+# run Single_Sample_GSEA_ssGSEA_fgsea.R
+lapply(gtm.dbs, function(gtm.db) {
+  # gtm.db <- gtm.dbs[1]
+  outdir <- file.path(basedir, 'ssGSEA', basename(gtm.db))
+  if(!dir.exists(outdir)) dir.create(outdir, recursive = T)
+
+  file.copy(file.path(basedir, 'logFC_geneClean.csv'), outdir)
+  setwd(outdir)
+  source(file.path(prjt_path, 'src/Single_Sample_GSEA_ssGSEA_fgsea.R'))
+
+  ssGSEA(gmtfile=gtm.db,fileranks=fileranks,Ptype=Ptype,pval_cutoff=pval_cutoff)
+  file.remove('logFC_geneClean.csv')
+
+  })
