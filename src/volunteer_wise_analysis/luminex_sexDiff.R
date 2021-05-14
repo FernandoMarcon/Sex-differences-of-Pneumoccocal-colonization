0921c5aa379181f5a32562fb0f895ee2a4cc2a59
FernandoMarcon <fmarcon13@gmail.com>
2021-05-14 17:03:40 -0300

    l

--- src/volunteer_wise_analysis/luminex_sexDiff.R
+++ src/volunteer_wise_analysis/luminex_sexDiff.R
@@ -44,3 +44,28 @@ kurstal.test %>% filter(p < .1) %>%
 luminex %>% gather('cytokine', 'value', na.rm = T) %>% ggplot(aes(value, group = cytokine, col = cytokine)) + geom_density(show.legend = F)
 
 prcomp(luminex)$x %>% data.frame %>% ggplot(aes(PC1,PC2)) + geom_point()
+
+luminex <- read.delim(file.path(basedir, 'logFC_luminex.csv'))
+nes <- read.delim('/home/marcon/Documents/work/Sex-differences-of-Pneumoccocal-colonization/intermediate/volunteer_wise_analysis/ssGSEA/WikiPathways_2019_Human/NES_logFC_geneClean.csv', row.names = 1)
+pheno <- read.delim(file.path(basedir, 'logFC_pheno.csv'))
+
+colnames(nes) <- gsub('\\.','\\/',gsub('X','',colnames(nes)))
+luminex <- luminex %>% unite('sample_id', volunteer_id, timepoint, sep = '_') %>% column_to_rownames('sample_id')
+pheno <- pheno %>% separate(class, c('dataset', 'carriage', 'sex'), sep = '_')
+
+cytokine.name = 'IL_6'
+pathway.name = 'IL-6 Signaling Pathway'
+
+nes = nes %>% rownames_to_column('pathway') %>% gather(volunteer_id, NES, -pathway) %>%
+  filter(grepl(gsub(' .*','',pathway.name), pathway))
+head(nes)
+luminex.sub <- luminex[,cytokine.name, drop = F] %>% setNames('cytokine') %>% rownames_to_column('sample_id') %>%
+  separate(sample_id, c('volunteer_id', 'timepoint'), sep = '_') %>% filter(complete.cases(cytokine))
+
+cytokine <- merge(nes, luminex.sub, by = 'volunteer_id')
+cytokine <- merge(pheno, cytokine, by = 'volunteer_id')
+head(cytokine)
+
+ggplot(cytokine, aes(NES, cytokine, col = sex, group = timepoint)) + geom_point(size = 2) +
+  facet_grid(dataset~timepoint) + theme(legend.position = 'top') +
+  labs(y = paste0(cytokine.name, ' (luminex)'), x= paste0(pathway.name, ' (NES)'))
