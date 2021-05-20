47ed2a3ec822f40cb96dbefa3ec08ab3fdbf322a
FernandoMarcon <fmarcon13@gmail.com>
2021-05-20 19:48:07 -0300

    CYPHER

--- src/volunteer_wise_analysis/cemitool/cemitool_neo4j.py
+++ src/volunteer_wise_analysis/cemitool/cemitool_neo4j.py
@@ -6,33 +6,12 @@ df.head()
 
 graph = Graph("bolt://localhost:7687", user="neo4j", password="cemitool")
 
-tx = graph.begin()
-for gene in df.genes.unique():
-    tx.run('CREATE (n:Gene {name: $gene})', parameters = {'gene': gene})
-for mod in df.modules.unique():
-    tx.run('CREATE (n:Module {name: $module})', parameters = {'module': mod})
-for index, row in df.iterrows():
-    tx.run('''
-       MATCH (a:Gene {name:$gene}), (b:Module {name:$module})
-       MERGE (a)-[r:MEMBER_OF]->(b)
-       ''', parameters = {'gene': row['genes'], 'module': row['modules']})
-tx.commit()
-
 query = '''
-        MERGE (e:Gene), (p:Module)
-        ON CREATE SET e.name = $gene, p.name = $mod
-        ON MATCH SET e.name = $gene, p.name = $mod
+        MERGE (e:Gene {name: $gene})
+        MERGE (p:Module {name: $mod})
         CREATE (e) - [:MEMBER_OF] -> (p)
         '''
 tx = graph.begin()
 for index, row in df.iterrows():
     tx.run(query, parameters = {'gene':row['genes'], 'mod':row['modules']})
 tx.commit()
-
-# for index, row in df.iterrows():
-#     tx.run('''
-#         CREATE (p1:Gene { name: $gene })
-#         CREATE (p2:Module { name: $module })
-#         CREATE (p1)-[:MEMBER_OF]->(p2)
-#        ''', parameters = {'gene': row['genes'], 'module': row['modules']})
-# tx.commit()
